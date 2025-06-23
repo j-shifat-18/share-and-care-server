64116916d0ec7a2c722ca9e86e542416f29d2237
j-shifat-18 <info.jahirulsifat@gmail.com>
2025-06-23 23:59:22 +0600

    add foods post api

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
-
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.psjt8aa.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -19,16 +17,35 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const foodsCollection = client.db("share_and_care").collection("foods");
+
+
+    app.get('/foods' , (req , res)=>{
+      const result =  foodsCollection.find();
+      console.log(result);
+      res.send(result);
+    })
+
+
+    app.post('/foods' , async(req , res)=>{
+      const newFoodData = req.body;
+      const result = await foodsCollection.insertOne(newFoodData);
+      res.send(result);
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -36,11 +53,10 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-app.get('/', (req, res) => {
-  res.send('Share & Care!')
-})
+app.get("/", (req, res) => {
+  res.send("Share & Care!");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
