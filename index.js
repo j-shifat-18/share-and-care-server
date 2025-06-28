8ec78167bfabe96253e839aaf9e598bfe9f2a938
j-shifat-18 <info.jahirulsifat@gmail.com>
2025-06-28 21:56:43 +0600

    hide the firebase service key

--- index.js
+++ index.js
@@ -10,7 +10,9 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
-const serviceAccount = require("./firebase-admin-key.json");
+const decoded = Buffer.from(process.env.FB_SERVICE_KEY , 'base64').toString('utf8');
+
+const serviceAccount = JSON.parse(decoded);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
